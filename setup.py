05b5851e854429d72ecaf5b206ed64ce55fae677
ashawkey <ashawkey1999@gmail.com>
2024-01-19 10:10:46 +0800

    fix long desc

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name='envlight', # package name, import this to use python API
     version='0.1.0',
     description='environment lighting toolkit',
+    long_description=open("readme.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     url='https://github.com/ashawkey/envlight',
     author='kiui',
     author_email='ashawkey1999@gmail.com',
